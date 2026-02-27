e4b20eebf3afee20ed1ea3467b971d9213e088d4
far-id <fcrizkywijaya@gmail.com>
2026-02-28 01:02:28 +0700

    add social authentication with GitHub and Google

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -4,8 +4,19 @@ import { db } from "@/db";
 import { tanstackStartCookies } from "better-auth/tanstack-start";
 
 export const auth = betterAuth({
+  baseURL: process.env.BETTER_AUTH_URL,
   emailAndPassword: {
     enabled: true,
+  },
+  socialProviders: {
+    github: {
+      clientId: process.env.GITHUB_CLIENT_ID as string,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET as string,
+    },
+    google: {
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+    },
   },
   database: drizzleAdapter(db, {
     provider: "pg",
